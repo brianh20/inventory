007dfc27b1432312890daca04334bc28d76c83d7
Brian Hume <brian.hume@globant.com>
2016-11-22 16:45:03 -0300

    routing and components

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,66 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.router']);
 
-app.directive('higuain', function(){
+app.config(function($stateProvider){
+	//$urlRouteProvider.otherwise("/");
+
+	var home = {
+		name: 'home',
+		url: '',
+		templateUrl : '/app/pages/home.html'
+	}
+
+	var newSensor = {
+		name: 'newSensor',
+		url: '/newSensor',
+		templateUrl : '/app/pages/newSensor.html'
+	}
+
+	var newElement = {
+		name: 'newElement',
+		url: '/newElement',
+		templateUrl : '/app/pages/newElement.html'
+	}
+
+	var viewSensors = {
+		name: 'viewSensors',
+		url: '/viewSensors',
+		templateUrl : '/app/pages/viewSensors.html'
+	}
+
+
+	$stateProvider.state(home);
+	$stateProvider.state(newSensor);
+	$stateProvider.state(newElement);
+	$stateProvider.state(viewSensors);
+	
+});
+
+app.directive('navbar', function(){
   return{
-    template: "Hola pipita"
+    templateUrl : '/app/widgets/navbar.html'
   };
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
