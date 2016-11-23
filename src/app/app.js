8cb8951c5993bd9d91690f4cdafde59be061a0f1
Brian Hume <brian.hume@globant.com>
2016-11-23 14:59:53 -0300

    clean responsive and mapped

--- src/app/app.js
+++ src/app/app.js
@@ -56,6 +56,32 @@ app.directive('navbar', function(){
   };
 });
 
+app.controller('editElement', function($scope) {
+
+	angular.element(document).ready(function () {
+		var expected = {lat: -34.6160073, lng: -58.3890407};
+		var truth = {lat: -34.617, lng: -58.3990407};
+
+		var map = new google.maps.Map(document.getElementById('elementMap'), {
+	  		zoom: 14,
+	  		center: expected
+		});
+		
+		var markerExpected = new google.maps.Marker({
+	  		position: expected,
+	  		map: map,
+	  		icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+		});
+		
+		var markerTruth = new google.maps.Marker({
+	  		position: truth,
+	  		map: map,
+	  		icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+		});
+    });
+
+});
+
 
 
 
